145438a6859748568fcf07f2a498c3fe1d429512
itayz1e <itayz1e@gmail.com>
2024-01-29 21:57:23 +0200

    Utilization and handling of all server side functions

--- API/web/webCardControl.ts
+++ API/web/webCardControl.ts
@@ -2,24 +2,63 @@ import * as dotenv from "dotenv";
 import WebCardModel from "./WebCardModel";
 dotenv.config();
 
-export async function addWebCard (req: any, res: any) {
-    try {
-      const { webName , webStatus } = req.body;
-      const webCardDB = await WebCardModel.create({ webName , webStatus });
+// Create a card
+export async function addWebCard(req: any, res: any) {
+  try {
+    const { webName, webStatus } = req.body;
+    const webCardDB = await WebCardModel.create({ webName, webStatus });
 
-      res.send({ ok: true, webCard: webCardDB });
-    } catch (error:any) {
-      console.error(error);
-      res.status(500).send({ ok: false, error });
-    }
+    res.send({ ok: true, webCard: webCardDB });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ ok: false, error });
   }
+}
+// Get all the cards
+export async function getAllWebCards(req: any, res: any) {
+  try {
+    const webCardDB = await WebCardModel.find({});
+    res.send({ ok: true, webCardDB });
+  } catch (error: any) {
+    console.error("cant get webCards");
+    res.status(500).send({ ok: false, error });
+  }
+}
 
-  export async function getAllWebCards (req: any, res: any){
-    try {
-        const webCardDB = await WebCardModel.find({});
-        res.send({ ok: true, webCardDB });
-    } catch (error:any) {
-        console.error("cant get webCards");
-      res.status(500).send({ ok: false, error });
+// Update one cards by id
+export const UpdateWebCardDetails = async (req: any, res: any) => {
+  try {
+    const { webName, webStatus } = req.body;
+    const _id = req.query._id;
+
+    const webCardDB = await WebCardModel.findByIdAndUpdate(_id, {
+      webName,
+      webStatus,
+    });
+
+    if (!webCardDB) {
+      throw new Error("No webCardDB found for the provided ID");
     }
+
+    res.send({ ok: true });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
+};
+
+// delete one card by id
+export const deleteWebCardById = async (req: any, res: any) => {
+  try {
+    const { _id } = req.query;
+    if (!_id) throw new Error("Web Card id not found");
+    const webCardDB = await WebCardModel.findByIdAndDelete(_id);
+
+    if (!webCardDB) throw new Error("web CardDB not found");
+    res.status(201).send({ ok: true, webCardDB: webCardDB });
+  } catch (error) {
+    res.status(500).send({ ok: false });
+
+    console.error(error);
+  }
+};
--- API/web/webCardRouter.ts
+++ API/web/webCardRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { addWebCard, getAllWebCards } from "./webCardControl";
+import { addWebCard, getAllWebCards, UpdateWebCardDetails, deleteWebCardById } from "./webCardControl";
 
 router
     .post("/add-web-card", addWebCard)
     .get("/get-web-cards", getAllWebCards)
+    .put("/update-web-card", UpdateWebCardDetails)
+    .delete("/delete-web-card", deleteWebCardById)
 
 export default router;
